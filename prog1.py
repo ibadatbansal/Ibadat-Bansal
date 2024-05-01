aabf82e9e5e63c47248d40218356a43c7ee7bc2b
ibadatbansal <168669392+ibadatbansal@users.noreply.github.com>
2024-05-01 23:08:46 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,18 @@
+print("Hi Lila, it's UNNAATI! Let me know what you're experiencing:")
+abdominalpain = input("Are you experiencing abdominal pain, cramps, bloating or swelling?")
+headache = input("Are you experiencing a headache?")
+vomiting = input("Are you experiencing vomiting, weakness and constant body aches?")
+fever = input("Are feeling ill, like having runny nose, and sore throat?")
+acne = input("Is your skin feeling ichy, oily and uncomfortable?")
+if abdominalpain =="t":
+    print("Press the PURPLE option, Here is your loperamide ")
+if headache =="t":
+    print("Press the BLUE option, Here is your aspirin")
+if vomiting =="t":
+    print("Press the BROWN option, Here is your aprepitant")
+if fever =="t":
+    print("Press the GREEN option, Here is your paracetamol")
+if acne =="t":
+    print("Press the BLACK option, Here is your tetracycline")
+print("Get well soon dear friend Lila! Love from UNNAATI <3")
+
